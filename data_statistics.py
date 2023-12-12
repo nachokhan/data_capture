8a2029f6859918d93fb636cd0e18582d2c8b8bdf
nachokhan <ignacio.rigoni@gmail.com>
2023-12-12 15:09:41 -0300

    added method to check number range

--- data_statistics.py
+++ data_statistics.py
@@ -13,14 +13,22 @@ class DataStatistics():
         self._update_stats(data)
 
     def less(self, number) -> int:
+        self._check_number(number)
         return self._less[number]
 
     def greater(self, number) -> int:
+        self._check_number(number)
         return self._greater[number]
 
     def between(self, n1, n2) -> int:
+        self._check_number(n1)
+        self._check_number(n2)
         out_of_range = self.greater(n2) + self.less(n1)
         return self._greater[0] - out_of_range
+
+    def _check_number(self, number):
+        if not (0 <= number < len(self._data)):
+            raise ValueError()
 
     def _update_stats(self, data: List) -> None:
         self._less = self._generate_cumulative_sum(data)
