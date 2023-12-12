b3cafca925e5d13b4afac7f8ff8a6ff0de3adb74
nachokhan <ignacio.rigoni@gmail.com>
2023-12-12 15:20:32 -0300

    added method comments for devs

--- data_capture.py
+++ data_capture.py
@@ -2,11 +2,32 @@ from data_statistics import DataStatistics
 
 
 class DataCapture():
+    """
+    DataCapture class is used to capture and store integer values in a fixed-size array.
+    It provides methods to add integer values and build statistics based on the stored data.
+
+    Attributes:
+        numbers (List[int]): A list to store the counts of integer values at their respective indices.
+    """
 
     def __init__(self) -> None:
+        """
+        Initialize a DataCapture object with a fixed-size array to store integer values.
+        The array has a size of 1000, representing values from 0 to 999.
+        """
         self.numbers = [0] * 1000
 
     def add(self, number: int) -> None:
+        """
+        Add an integer value to the storage array.
+
+        Args:
+            number (int): The integer value to be added.
+
+        Raises:
+            TypeError: If the input value is not an integer.
+            ValueError: If the input value is not in the range [0, 999].
+        """
         if not isinstance(number, int):
             raise TypeError("Value is not integer")
 
@@ -16,4 +37,11 @@ class DataCapture():
         self.numbers[number] += 1
 
     def build_stats(self):
+        """
+        Build statistics based on the stored data and return a DataStatistics object.
+
+        Returns:
+            DataStatistics: An instance of the DataStatistics class containing statistics
+            for the stored data.
+        """
         return DataStatistics(self.numbers)
